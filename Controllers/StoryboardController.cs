a2fa34fe77b2c7d5fa3dc3bba716d865cb8d8bdf
noemiemathu <152178258+noemiemathu@users.noreply.github.com>
2025-07-01 09:25:31 +0200

    Add files via upload

---
+++ Controllers/StoryboardController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Storyboard2.Models;
+
+namespace StoryBoard2.Controllers
+{
+    public class StoryboardController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Change(int number, int selectedPanel, string title)
+        {
+            var model = new Story { Number = number, SelectedPanel = selectedPanel, Title = title };
+            return View("Change", model);
+        }
+        public ActionResult Show()
+        {
+            return View("Show");
+        }
+    }
+}
---
+++ Models/Story.cs
@@ -0,0 +1,10 @@
+﻿namespace Storyboard2.Models
+{
+    public class Story
+    {
+        public int Number { get; set; }
+        public int SelectedPanel { get; set; }
+        public string Title = "Titel";
+    }
+
+}
